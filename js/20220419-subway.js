f8373dd4cfb5fb8c3942071068e055a46b6a68d6
dobbang <seung0540@hanmail.net>
2023-11-03 14:16:59 +0900

    서브웨폰트

--- js/20220419-subway.js
+++ js/20220419-subway.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $("document").ready(function(){
 
     
@@ -9,16 +8,4 @@ $("document").ready(function(){
         },
     })
 
-=======
-$("document").ready(function(){
-
-    
-    const swiper = new Swiper(".slide-section", {
-        loop: true,
-        autoplay: { 
-            delay: 2000 
-        },
-    })
-
->>>>>>> aa1c03e336f18647f93b3bbc197ef97d07860800
 })
