284cc62beab1a03713998fca262ba2f48f416df9
dobbang <seung0540@hanmail.net>
2024-02-28 21:05:45 +0900

    240228

--- js/q-nent.js
+++ js/q-nent.js
@@ -1,9 +1,37 @@
 $("document").ready(function(){
 
 
-    $(".box").click(function() {
-
-        });
 
 
+
+    const mainswiper = new Swiper(".main-slider",{
+        loop: true,
+        autoplay: { 
+            delay: 2000
+        },
+        navigation:{
+            nextEl:".swiper-button-next",
+            prevEl:" .swiper-button-prev"
+        },
+        effect: "fade",
+        
+        })
+
+
+        // tab-menu
+
+        $('.tab-title-ul li').eq(0).addClass('on')
+        $('.tab-text .tab-text-content').eq(0).addClass('on')
+        
+        $('.tab-title-ul li').click(function(){
+            
+            const k = $(this).index();
+            $('.tab-title-ul li').removeClass("on").eq(k).addClass("on")
+            $('.tab-text-content').removeClass("on").eq(k).addClass("on")
+
+
+        })
+
+
+        
 });
