903df2409572e9c57fc6c4137af54beeecc335c9
dobbang <seung0540@hanmail.net>
2024-04-12 18:13:39 +0900

    240412

--- js/KBbank.js
+++ js/KBbank.js
@@ -24,23 +24,15 @@ $("document").ready(function(){
 
   $(".tab-menu ul li").eq(0).addClass("on")
 
-    // 화면 처음 시작시 첫번째 탭 컨텐츠를 화면에서 출력
-
     $(".tab-wrap .tab-content").eq(0).show()
-
-    //탭 리스트를 클릭하는 이벤트생성 , 현재의 인덱스 값을 변수에 저장 ,
 
     $(".tab-menu ul li").click(function(){
         let k = $(this).index();
-        //탭리스트 클릭시 리스트 아이템의 클래스를 삭제하고 선택한 요소에 클래스 추가
       
         $(".tab-menu ul li").removeClass("on").eq(k).addClass("on")
     
-        // 탭리스트 클릭시 탭 컨텐츠를 화면에서 모두 지우고 선택한 요소를 화면에 출력
-    
         $(".tab-menu .tab-content").hide().eq(k).show();
 
-        //어디서 선택자 끝나는지 잘보기 오타 잘보기 
     })
 
 
@@ -51,6 +43,15 @@ $("document").ready(function(){
             delay: 2000 
         },
     })
+
+    const Sswiper = new Swiper(".solution-slider", {
+      loop: true,
+      slidesPerView: 3,  
+      autoplay: { 
+          delay: 2000 
+      },
+      spaceBetween: 30, //마진
+  })
 
 
 
